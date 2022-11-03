5f96ac14278045c12da138380381b89b61f8ea48
sinlernick <111036791+sinlerdev@users.noreply.github.com>
2022-11-03 21:02:31 +0200

    resolveDirections.lua

--- src/resolveDirections.lua
+++
@@ -1,7 +0,0 @@
-return function(Vector: Vector3)
-	--[[
-        We round the axies from the given vector to avoid unwanted results like (0.01, 0.001, 0.02)
-    ]]
-
-	return Vector3.new(math.round(Vector.X), math.round(Vector.Y), math.round(Vector.Z))
-end
